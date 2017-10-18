bcbe2fe80cc357faa40f048f1026cfee8746a493
Antti Leppä <antti.leppa@metatavu.fi>
2017-10-18 13:10:00 +0300

    Added support for put and post bodies

--- client-extras/src/main/java/fi/metatavu/ptv/client/ApiClient.java
+++ client-extras/src/main/java/fi/metatavu/ptv/client/ApiClient.java
@@ -6,9 +6,9 @@ public abstract class ApiClient {
   
   public abstract <T> ApiResponse<T> doGETRequest(String path, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
   
-  public abstract <T> ApiResponse<T> doPOSTRequest(String path, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
+  public abstract <T> ApiResponse<T> doPOSTRequest(String path, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams, Object body);
   
-  public abstract <T> ApiResponse<T> doPUTRequest(String path, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
+  public abstract <T> ApiResponse<T> doPUTRequest(String path, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams, Object body);
   
   public abstract <T> ApiResponse<T> doDELETERequest(String path, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
   
