3a6f1bc109fc401a3b0f910dba4ccc766cf4354b
Antti Leppä <antti.leppa@metatavu.fi>
2017-10-19 16:09:53 +0300

    Added support for using access tokens

--- client-extras/src/main/java/fi/metatavu/ptv/client/ApiClient.java
+++ client-extras/src/main/java/fi/metatavu/ptv/client/ApiClient.java
@@ -4,12 +4,12 @@ import java.util.Map;
 
 public abstract class ApiClient {
   
-  public abstract <T> ApiResponse<T> doGETRequest(String path, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
+  public abstract <T> ApiResponse<T> doGETRequest(String accessToken, String path, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
   
-  public abstract <T> ApiResponse<T> doPOSTRequest(String path, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams, Object body);
+  public abstract <T> ApiResponse<T> doPOSTRequest(String accessToken, String path, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams, Object body);
   
-  public abstract <T> ApiResponse<T> doPUTRequest(String path, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams, Object body);
+  public abstract <T> ApiResponse<T> doPUTRequest(String accessToken, String path, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams, Object body);
   
-  public abstract <T> ApiResponse<T> doDELETERequest(String path, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
+  public abstract <T> ApiResponse<T> doDELETERequest(String accessToken, String path, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
   
 }
