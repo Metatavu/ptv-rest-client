a99386a4db726502c9b3674e5742e1dc41012808
Antti Leppä <antti.leppa@metatavu.fi>
2019-07-08 15:21:06 +0300

    ptv10

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const util = require('util');
 
 const SWAGGER_VERSION = "2.2.3";
-const PTV_VERSION = 'v9';
+const PTV_VERSION = 'v10';
 const SWAGGER_URL = util.format('https://api.palvelutietovaranto.trn.suomi.fi/swagger/%s/swagger.json', PTV_VERSION);
 
 module.exports = function(grunt) {
